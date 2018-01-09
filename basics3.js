d216210c54413bc5a5c686d8ab965e7d98e95760
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-08 20:29:16 -0600

    Done with Math 2

--- basics3.js
+++ basics3.js
@@ -9,11 +9,28 @@ function zero_negativity(array) {
   return true;
 }
 
-console.log(zero_negativity([-1]));
-console.log(zero_negativity([]));
-console.log(zero_negativity([0]));
-console.log(zero_negativity([1]));
-console.log(zero_negativity([1,2]));
-console.log(zero_negativity([-1,2]));
+console.log("Start of zero_negativity Tests");
+console.log(zero_negativity([-1])); // false
+console.log(zero_negativity([])); // true
+console.log(zero_negativity([0])); // true
+console.log(zero_negativity([1])); // true
+console.log(zero_negativity([1,2])); // true
+console.log(zero_negativity([-1,2])); // false
+console.log();
 
 // Math 2
+function is_even(number) {
+  if (number % 2 === 0)
+    return true;
+  else
+    return false;
+}
+console.log("Start of is_even Tests");
+console.log(is_even()); // false
+console.log(is_even(0)); // true
+console.log(is_even(2)); // true
+console.log(is_even(1)); // false
+console.log(is_even(-1)); // false
+console.log(is_even(-2)); // true
+console.log();
+
